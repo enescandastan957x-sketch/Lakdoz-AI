ac4b6eaea412accd5834b232bd7f1599e13a9a61
enescandastan957x-sketch <enescandastan957x@gmail.com>
2026-08-26 21:20:13 +0300

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.lakdoz.assistant"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.lakdoz.assistant"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 50
+        versionName = "0.5.0"
+    }
+}

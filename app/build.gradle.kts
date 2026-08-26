68fc93cbb1a3cf8906604f7652ec9ca193f26ba3
enescandastan957x-sketch <enescandastan957x@gmail.com>
2026-08-26 22:45:42 +0300

    Release Lakdoz 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.lakdoz.assistant"
         minSdk = 29
         targetSdk = 35
-        versionCode = 101
-        versionName = "0.10.1"
+        versionCode = 110
+        versionName = "1.0.0"
     }
 }

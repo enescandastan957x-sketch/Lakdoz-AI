380852a141b2d611b6f936f4b1701d023c491169
enescandastan957x-sketch <enescandastan957x@gmail.com>
2026-08-26 23:16:50 +0300

    Bump Lakdoz to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.lakdoz.assistant"
         minSdk = 29
         targetSdk = 35
-        versionCode = 113
-        versionName = "1.0.3"
+        versionCode = 114
+        versionName = "1.0.4"
     }
 }
